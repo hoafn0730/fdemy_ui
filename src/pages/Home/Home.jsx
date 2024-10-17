3082a7e023c20e3d21abd643ae7be8b07eb3f08b
hoafn0730 <hoantran0730@gmail.com>
2024-10-17 22:15:20 +0700

    update loading

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import classnames from 'classnames/bind';
 import { useEffect, useState } from 'react';
+import { DotLoader } from 'react-spinners';
 
 import styles from './Home.module.scss';
 import IndexModule from '~/components/IndexModule';
@@ -12,12 +13,20 @@ const cx = classnames.bind(styles);
 
 function Home() {
     const [courses, setCourses] = useState();
+    let [loading, setLoading] = useState(true);
+
+    const override = {
+        display: 'block',
+        margin: '0 auto',
+        textAlign: 'center',
+    };
 
     const fetchApi = () => {
         courseService
             .getCourses()
             .then((res) => {
                 setCourses(res);
+                setLoading(false);
             })
             .catch((err) => console.log(err));
     };
@@ -28,46 +37,65 @@ function Home() {
 
     return (
         <IndexModule className={cx('grid', 'wide')}>
-            <div className={cx('slideshow')}>
-                <Slideshow />
-            </div>
-            <div className={cx('wrapper')}>
-                <ScrollList
-                    title={'Khóa học Pro'}
-                    render={() =>
-                        courses &&
-                        courses?.proCourses.map(({ title, image, ctaTitle, oldPrice, price, slug }, index) => (
-                            <CourseItem
-                                key={index}
-                                title={title}
-                                linkTo={'/courses/' + slug}
-                                image={image}
-                                ctaTitle={ctaTitle}
-                                oldPrice={oldPrice}
-                                mainPrice={price}
-                            />
-                        ))
-                    }
-                />
-                <ScrollList
-                    title={'Khóa học Free'}
-                    subTitle={'386.894+ người khác đã học'}
-                    render={() =>
-                        courses &&
-                        courses?.freeCourses.map(({ title, image, ctaTitle, oldPrice, mainPrice, slug }, index) => (
-                            <CourseItem
-                                key={index}
-                                title={title}
-                                linkTo={'/courses/' + slug}
-                                image={image}
-                                ctaTitle={ctaTitle}
-                                oldPrice={oldPrice}
-                                mainPrice={mainPrice}
-                            />
-                        ))
-                    }
-                />
-            </div>
+            {loading && (
+                <div style={{ height: '90vh', display: 'flex', alignItems: 'center' }}>
+                    <DotLoader
+                        color={'#38d6b7'}
+                        loading={loading}
+                        cssOverride={override}
+                        size={100}
+                        aria-label="Loading Spinner"
+                        data-testid="loader"
+                    />
+                </div>
+            )}
+
+            {!loading && (
+                <>
+                    <div className={cx('slideshow')}>
+                        <Slideshow />
+                    </div>
+                    <div className={cx('wrapper')}>
+                        <ScrollList
+                            title={'Khóa học Pro'}
+                            render={() =>
+                                courses &&
+                                courses?.proCourses.map(({ title, image, ctaTitle, oldPrice, price, slug }, index) => (
+                                    <CourseItem
+                                        key={index}
+                                        title={title}
+                                        linkTo={'/courses/' + slug}
+                                        image={image}
+                                        ctaTitle={ctaTitle}
+                                        oldPrice={oldPrice}
+                                        mainPrice={price}
+                                    />
+                                ))
+                            }
+                        />
+                        <ScrollList
+                            title={'Khóa học Free'}
+                            subTitle={'386.894+ người khác đã học'}
+                            render={() =>
+                                courses &&
+                                courses?.freeCourses.map(
+                                    ({ title, image, ctaTitle, oldPrice, mainPrice, slug }, index) => (
+                                        <CourseItem
+                                            key={index}
+                                            title={title}
+                                            linkTo={'/courses/' + slug}
+                                            image={image}
+                                            ctaTitle={ctaTitle}
+                                            oldPrice={oldPrice}
+                                            mainPrice={mainPrice}
+                                        />
+                                    ),
+                                )
+                            }
+                        />
+                    </div>
+                </>
+            )}
         </IndexModule>
     );
 }
